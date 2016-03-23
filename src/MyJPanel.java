f2fbae39ace8eba4df96a74c5c1c24ad3474ef54
jessica <jessica>
2016-03-24 01:31:18 +0800

    version1 - can move the square and change the color

--- src/MyJPanel.java
+++ src/MyJPanel.java
@@ -9,6 +9,7 @@ import javax.swing.*;
 public class MyJPanel extends JPanel implements MouseMotionListener{
 	
 	private Square square = new Square(100);
+	private Point mouse = new Point(50, 50);
 	
 	public MyJPanel(){
 		addMouseMotionListener(this);
@@ -17,13 +18,17 @@ public class MyJPanel extends JPanel implements MouseMotionListener{
 		super.paintComponent(g);
 		// TODO Draw square and fill it with random color decided by method getRandomColor()
 		// You should use fillRect()
+		g.setColor(square.getRandomColor());
+		g.fillRect(square.getCenterX(mouse.x), square.getCenterY(mouse.y), square.getShapeWidth(), square.getShapeHeight());
+		repaint();
 		
 	}
 
 	@Override
 	public void mouseDragged(MouseEvent e) {
 		// TODO Get mouse dragged position and change suqare's position
-		
+		mouse.x = e.getXOnScreen();
+		mouse.y = e.getYOnScreen();
 	}
 
 	@Override
--- src/Square.java
+++ src/Square.java
@@ -7,7 +7,7 @@ public class Square extends Shape {
 	
 	public Square(int shapeWidth){
 		// TODO Constructor
-		
+		this.shapeWidth = shapeWidth;
 	}
 	
 	
@@ -16,33 +16,33 @@ public class Square extends Shape {
 		// TODO Java 'Color' class takes 3 floats, from 0 to 1.
 		
 		// TODO Return color produced by three rgb floats.
-		return new Color(r, g, b);
+		return new Color((float)Math.random(),(float) Math.random(), (float)Math.random());
 	}
 	
 	@Override
 	public int getShapeWidth(){
 		// TODO Return Square width
-		return 0;
+		return shapeWidth;
 	}
 	
 	@Override
 	public int getShapeHeight(){
 		// TODO Return Square width
-		return 0;
+		return shapeWidth;
 	}
 
 
 	@Override
 	public int getCenterX(int mouseX) {
 		// TODO Input mouse X position and return center X of square
-		return 0;
+		return mouseX - shapeWidth;
 	}
 
 
 	@Override
 	public int getCenterY(int mouseY) {
 		// TODO Input mouse Y position and return center Y of square
-		return 0;
+		return mouseY - shapeWidth;
 	}
 
 
